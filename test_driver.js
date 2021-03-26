8972b40c8a90f6bbd00819174a4dfae8f2351eb0
mizuka-ninomae <mizuka_ninomae@icloud.com>
2021-03-26 23:16:23 +0900

    update

--- test_driver.js
+++ test_driver.js
@@ -1,6 +1,6 @@
 const Che_TPHU5     = require('sanwa_che-tphu5');
 let  blu_mac        = "XX:XX:XX:XX:XX:XX";
-let  noble_ctl_path = "/home/pi/sanwa_che-tphu5/";
+let  noble_ctl_path = "/home/pi/homebridge-sensor/node_modules/sanwa_che-tphu5/";
 
 let wosendor = new Che_TPHU5 (blu_mac, noble_ctl_path, function (error, value) {
     console.log (value);
